1331db5c5ca0e75003c7743582afc6540393bae1
Lincoln Yan <yyjlincoln@gmail.com>
2020-10-06 00:18:11 +1100

    Bug Fix

--- client.py
+++ client.py
@@ -5,23 +5,24 @@ max_trial = 5
 trial = 0
 
 while True:
-    s = socket.socket()
-    s.connect(('localhost', 8080))
-
-    ctx = Events('TestClientA')
-
-    ctx.emit('echo', {
-        'payload!': 'stuff'
-    }, s).then(print)
-
-    @ctx.response()
-    def res(head, body):
-        print(body)
-
     try:
+        s = socket.socket()
+        s.connect(('localhost', 8080))
+
+        ctx = Events('TestClientA')
+
+        ctx.emit('echo', {
+            'payload!': 'stuff'
+        }, s).then(print)
+
+        @ctx.response()
+        def res(head, body):
+            print(body)
+    
         ctx.listen(s)
         # This will block all requests until an exception is thrown
-    except:
+    except Exception as e:
+        print(e)
         trial += 1
         if trial == max_trial:
             break
